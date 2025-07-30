6589049e52d527f77d1e12ffd30546f76f96701c
Arslan-34 <manarslan4@gmail.com>
2025-07-30 17:32:06 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Multilingual TTS Demo',
+      title: 'Multilingual TTS Demo by Arslan',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
